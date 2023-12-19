c49c479f6ee83e117ecf8b644d904e185646739b
Wilson de Melo Ribeiro <106207965+WilsonMeloRibeiro@users.noreply.github.com>
2023-12-19 07:22:25 -0300

    deleted useless files

--- config/authorizedRoles.js
+++
@@ -1,6 +0,0 @@
-const rolesList ={
-    "user":200,
-    "editor":201,
-    "admin":202
-}
-module.exports = rolesList
--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const jwtVerify = require('../middleware/jwtVerify');
 const handleLogin = require('../controllers/authController');
-const handleRefresh = require('../controllers/refreshController');
-const handleRolesVerify = require('../middleware/rolesVerify')
-const authorizedRoles = require('../config/authorizedRoles.js')
-const handleLogout = require('../controllers/logoutController')
 const productsController = require('../controllers/productsController.js')
 
 router.post('/auth', handleLogin);
-router.get('/auth', handleRefresh);
-
-router.get('/logout', handleLogout);
 
 router.get('/products', productsController.getProducts)
 router.post('/products', productsController.registerProduct)
